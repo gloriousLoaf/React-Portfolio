54110ade2da5926bf194116a53b139d32625be9c
David Metcalf <davidmetcalfwork@gmail.com>
2020-05-21 12:17:23 -0700

    Added more content to Owl Carousel on Portfolio page. Testing a .resize() listener to append Swipe instruction for Mobile screens.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,28 @@ $('.owl-carousel').owlCarousel({
             items: 1,
             slideBy: 1
         },
-        600: {
+        575: {
             items: 3,
             slideBy: 3
         }
     }
 })
+
+// Window Listener to append or remove Swipe instructions for Mobile screens
+// thanks to Cory Danielson & JT on Stack Overflow for guide:
+// https://stackoverflow.com/questions/9828831/jquery-on-window-resize
+// https://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed
+
+// Underscore.js .debounce() to prevent multiple firings
+$(window).resize(_.debounce(function () {
+    let win = $(this);
+    // var to hold html
+    let $swipeMe = '<div class="row d-flex justify-content-center swipe-me"><div class="card"><div class="card-body">Swipe or Tap Arrows</div></div></div>';
+    // conditional: if under 575 append html, if over remove
+    if (win.width() <= 575) {
+        $(".card-deck").append($swipeMe);
+    }
+    if (win.width() >= 575) {
+        $(".swipe-me").remove();
+    }
+}, 500));
