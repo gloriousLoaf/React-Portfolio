073140706282c7970c2e668c7df3bdf10545a7a2
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-19 11:56:57 -0700

    Removed CRA boilerplate. Created basic file structure and light wireframe.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hero from './components/Hero/index.js';
+// import Nav from './components/Nav/index.js';
+// import Portfolio from './components/Portfolio/index.js';
+// import PortCard from './components/PortCard/inde.js';
+// import About from './components/About/index.js';
+// import Skills from './components/Skills/index.js';
+// import Contact from './components/Contact/index.js';
+// import Thanks from './components/Thanks/index.js';
+// import Footer from './components/Footer/index.js';
 import './App.css';
 
+// just wireframing a bit. this will change a lot.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <div className="container">
+                <Hero />
+                {/* <Nav />
+                <Portfolio />
+                <About />
+                <Skills />
+                <Contact />
+                <Thanks />
+                <Footer /> */}
+            </div>
+        </div>
+    );
+};
 
 export default App;
