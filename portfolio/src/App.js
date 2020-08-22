5080468436ff0ae997638f3e5ffffc383a7b2904
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-22 10:25:37 -0700

    Added Switch to Router in App.js. Needs Error path.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 // App
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Wrapper from './components/Wrapper';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -18,9 +18,11 @@ function App() {
                 <Header />
                 <Navbar />
                 <Wrapper>
-                    <Route exact path="/" component={Portfolio} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" component={Contact} />
+                    <Switch >
+                        <Route exact path="/" component={Portfolio} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/contact" component={Contact} />
+                    </Switch>
                 </Wrapper>
                 <Footer />
             </div>
