c23a4b6f7af29e951575ff299fbd95161dd053cb
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-24 14:23:48 -0700

    Removed /core dir with the old portfolio, moved all React app dirs up one level. Deleted unused Wrapper component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // App
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Wrapper from './components/Wrapper';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Portfolio from './components/Portfolio';
-// import Project from './components/Project';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -17,13 +15,11 @@ function App() {
             <div className="App">
                 <Header />
                 <Navbar />
-                {/* <Wrapper> */}
                 <Switch >
                     <Route exact path="/" component={Portfolio} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/contact" component={Contact} />
                 </Switch>
-                {/* </Wrapper> */}
                 <Footer />
             </div>
         </Router>
