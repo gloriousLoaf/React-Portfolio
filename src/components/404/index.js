5422dc3fc1e5536a7fd5c7cadbd75b663a973141
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-25 11:30:02 -0700

    Fixed CSS conflict with header margins from 404 page affecting other headers.

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -9,8 +9,8 @@ function NotFound() {
         <div className="container">
             <div className="jumbotron">
                 <h1>404: Page Not Found</h1>
-                <h2>Dear Explorer,</h2>
-                <h5>You have reached the end of my interwebz. Head <NavLink exact to="/"
+                <h2 className="left">Dear Explorer,</h2>
+                <h5 className="left">You have reached the end of my interwebz. Head <NavLink exact to="/"
                     className="home">home?</NavLink><span role="img"
                         aria-label="emoji hand pointing left to Home link"> 👈</span></h5>
             </div>
